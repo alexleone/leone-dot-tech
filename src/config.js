64211a934114739a6855e941931a87ac041f39a5
Alex Leone <alexmleone@gmail.com>
2017-09-13 10:53:34 -0400

    Basic funcionailty for auth with congnito done, stubbed sign up, login, and create new note

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  cognito: {
+    USER_POOL_ID: "us-east-1_ywYSdSMfj",
+    APP_CLIENT_ID: "7itkfuoe1smkotj3hmk8jtal9i"
+  },
+  REGION: "us-east-1",
+  IDENTITY_POOL_ID: "us-east-1_ywYSdSMfj",
+  apiGateway: {
+    URL: "https://llmjysubbc.execute-api.us-east-1.amazonaws.com/prod",
+    REGION: "us-east-1"
+  },
+  MAX_ATTACHMENT_SIZE: 5000000,
+  s3: {
+    BUCKET: "leone-tech"
+  },
+};
